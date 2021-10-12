1fa89f5f2d3a3e136639eb834b791a76d2a4e0a1
Mazan Dali <dalimazan94@gmail.com>
2021-10-12 20:28:44 +0300

    home screen finished

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:task2/widgets/app_bar.dart';
 import 'package:task2/widgets/card_info.dart';
+
+final List<Map> cardlist2 = [
+  {
+    "imgelink": "assets/images/image_1.jpg",
+    "productname": "Product_1",
+    "productdetails": "Details_1",
+    "Price": "2000"
+  },
+  {
+    "imgelink": "assets/images/image_2.jpg",
+    "productname": "Product_2",
+    "productdetails": "Details_2",
+    "Price": "3000"
+  },
+  {
+    "imgelink": "assets/images/image_3.jpg",
+    "productname": "Product_3",
+    "productdetails": "Details_3",
+    "Price": "4000"
+  },
+  {
+    "imgelink": "assets/images/image_4.jpg",
+    "productname": "Product_4",
+    "productdetails": "Details_4",
+    "Price": "5000"
+  },
+  {
+    "imgelink": "assets/images/image_5.jpg",
+    "productname": "Product_5",
+    "productdetails": "Details_5",
+    "Price": "6000"
+  }
+];
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -46,8 +80,52 @@ class HomeScreen extends StatelessWidget {
             ],
           ),
         ),
-        const CardInfo()
+        Expanded(
+          child: StaggeredGridView.countBuilder(
+            crossAxisCount: 4,
+            itemCount: cardlist.length,
+            padding: const EdgeInsets.all(15),
+            itemBuilder: (context, index) => Expanded(
+                child: InkWell(
+              onTap: () {},
+              child: CardInfo(
+                  imageLink: cardlist[index],
+                  productname: "productname",
+                  productdetails: "productdetails",
+                  price: "price"),
+            )),
+            staggeredTileBuilder: (index) => const StaggeredTile.fit(2),
+            // StaggeredTile.count(2, index.isEven ? 2 : 1),
+            mainAxisSpacing: 30.0,
+            crossAxisSpacing: 30.0,
+          ),
+        ),
       ]),
     );
   }
 }
+
+
+    //  Expanded(
+    //       child: StaggeredGridView.countBuilder(
+    //         crossAxisCount: 4,
+    //         itemCount: cardlist.length,
+    //         padding: const EdgeInsets.all(10),
+    //         itemBuilder: (context, index) => Expanded(
+    //           child: cardlist2
+    //               .map(
+    //                 (e) => CardInfo(
+    //                   imageLink: e["imagelink"],
+    //                   productname: e["productname"],
+    //                   productdetails: e["productdetails"],
+    //                   price: e["Price"],
+    //                 ),
+    //               )
+    //               .toList(),
+    //         ),
+    //         staggeredTileBuilder: (index) => StaggeredTile.fit(2),
+    //         // StaggeredTile.count(2, index.isEven ? 2 : 1),
+    //         mainAxisSpacing: 10.0,
+    //         crossAxisSpacing: 10.0,
+    //       ),
+    //     ),
--- lib/widgets/card_info.dart
+++ lib/widgets/card_info.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+final List<String> cardlist = [
+  "assets/images/image_1.jpg",
+  "assets/images/image_2.jpg",
+  "assets/images/image_3.jpg",
+  "assets/images/image_4.jpg",
+  "assets/images/image_5.jpg"
+];
+final List<Map> cardlist2 = [
+  {
+    "imgelink": "assets/images/image_1.jpg",
+    "productname": "Product_1",
+    "productdetails": "Details_1",
+    "Price": "2000"
+  },
+  {
+    "imgelink": "assets/images/image_2.jpg",
+    "productname": "Product_2",
+    "productdetails": "Details_2",
+    "Price": "3000"
+  },
+  {
+    "imgelink": "assets/images/image_3.jpg",
+    "productname": "Product_3",
+    "productdetails": "Details_3",
+    "Price": "4000"
+  },
+  {
+    "imgelink": "assets/images/image_4.jpg",
+    "productname": "Product_4",
+    "productdetails": "Details_4",
+    "Price": "5000"
+  },
+  {
+    "imgelink": "assets/images/image_5.jpg",
+    "productname": "Product_5",
+    "productdetails": "Details_5",
+    "Price": "6000"
+  }
+];
 
 class CardInfo extends StatelessWidget {
-  const CardInfo({
-    Key? key,
-  }) : super(key: key);
-
+  const CardInfo(
+      {Key? key,
+      required this.imageLink,
+      required this.productname,
+      required this.productdetails,
+      required this.price})
+      : super(key: key);
+  final String imageLink;
+  final String productname;
+  final String productdetails;
+  final String price;
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
+      borderRadius: BorderRadius.circular(40),
       child: Container(
         height: 400,
         width: 200,
@@ -21,34 +69,40 @@ class CardInfo extends StatelessWidget {
               height: 250,
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(15),
-                child: Image.asset("assets/images/image_1.jpg"),
+                child: Image.asset(imageLink),
               ),
             ),
           ),
           const Padding(
             padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Text("Product Name"),
+            child: Text(
+              "productname",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ),
           const Padding(
             padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-            child: Text("Product Details"),
+            child: Text(
+              "productdetails",
+              style: TextStyle(fontSize: 15, color: Colors.grey),
+            ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text("200"),
+                const Text("Price", style: TextStyle(fontSize: 22)),
                 Container(
-                  width: 30.0,
-                  height: 30.0,
+                  // width: 30.0,
+                  // height: 30.0,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(50),
                       color: Colors.black),
                   child: IconButton(
                     onPressed: () {},
                     icon: const Icon(
-                      Icons.linked_camera,
+                      Icons.leak_remove,
                       color: Colors.white,
                     ),
                   ),
